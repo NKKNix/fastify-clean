7cb781cbb29235a6165e2cfac285d359b821b5cf
NKKNix <supawit.thongrueng@gmail.com>
2025-07-21 10:44:14 +0700

    feat: Update OpenTelemetry configuration for improved trace and metric handling, add Prometheus scrape configuration

--- src/infrastructure/otel/otelTracing.ts
+++ src/infrastructure/otel/otelTracing.ts
@@ -1,6 +1,7 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
@@ -12,6 +13,10 @@ const traceExporter = new OTLPTraceExporter({
 const metricExporter = new OTLPMetricExporter({
   url: 'http://localhost:4318/v1/metrics',
 });
+
+const logExporter = new OTLPLogExporter({
+  url : 'http://localhost:4318/v1/logs',
+})
 // 
 
 const sdk = new NodeSDK({
@@ -21,6 +26,7 @@ const sdk = new NodeSDK({
     exporter: metricExporter,
     exportIntervalMillis: 1000, // ลดเวลาลงเพื่อดีบักให้เห็นผลเร็ว
   }),
+  
   instrumentations: [getNodeAutoInstrumentations({
     
   }
